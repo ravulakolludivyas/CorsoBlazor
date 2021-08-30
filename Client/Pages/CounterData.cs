63213b67c747cda956a98216a906bb13e6e10af2
divya <divyaravulakollu@gmail.com>
2021-08-30 19:24:33 +0200

    Add project files.

---
+++ Client/Pages/CounterData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorsoBlazor.Client.Pages
+{
+    public class CounterData
+    {
+        private int count;
+        public int Count
+        {
+            get => this.count;
+            set
+            {
+                if(value!= this.count)
+                {
+                    this.count = value;
+                    CountChanged?.Invoke(this.count);
+                }
+            }
+        }
+        public Action<int> CountChanged { get; set; }
+    }
+}
+
---
+++ CorsoBlazor.Components/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.AspNetCore.Components;
+
+namespace CorsoBlazor.Components
+{
+    public class Timer: ComponentBase
+    {
+        [Parameter]
+        public double TimeInSeconds { get; set; }
+
+        [Parameter]
+        public EventCallback Tick { get; set; }
+        //public Action Tick { get; set; }
+
+        protected override void OnInitialized()
+        {
+            var timer = new System.Threading.Timer(
+                callback: (_) => InvokeAsync(() => Tick.InvokeAsync(null)),
+                state: null,
+                dueTime: TimeSpan.FromSeconds(TimeInSeconds),
+                period: Timeout.InfiniteTimeSpan);
+            
+        }
+    }
+}
